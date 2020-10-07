f0c65cf366de42dac36a39afe2c4ce829590d44d
Kai Stieler <kai.stieler@outloook.de>
2020-10-07 11:16:22 +0200

    return type void was missing

--- src/githubtest/Main.java
+++ src/githubtest/Main.java
@@ -1,9 +1,10 @@
 package githubtest;
 
 public class Main {
-	private static getricked() {
+	private static void getricked() {
 		for(int i = 0; i <= 10; i++) {
 		System.out.println("Never gonna give u up, never gonna let you down");
+	
 		}
 	}
 public static void main(String[] args) {
