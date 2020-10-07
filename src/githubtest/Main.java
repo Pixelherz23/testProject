999c65e5c4e95b0fbe14d478957aead855116757
Kai Stieler <kai.stieler@outloook.de>
2020-10-07 10:59:01 +0200

    Hot code in your area!

--- src/githubtest/Main.java
+++ src/githubtest/Main.java
@@ -1,7 +1,13 @@
 package githubtest;
 
 public class Main {
+	private static getricked() {
+		for(int i = 0; i <= 10; i++) {
+		System.out.println("Never gonna give u up, never gonna let you down");
+		}
+	}
 public static void main(String[] args) {
 	
 }
+
 }
